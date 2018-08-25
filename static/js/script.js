c443c59bb6c297b5d8ec806882832dcfbe8574ca
Arvind <arvind@zonic.io>
2018-08-25 22:53:47 +0530

    initial commit

---
+++ static/js/script.js
@@ -0,0 +1,32 @@
+/* 
+==============================================
+* Green Paper - v1.0
+==============================================
+
+* Demo: https://demo.uicard.io/green-paper-pro/
+* Free Version: https://uicard.io/products/green-paper
+* Premium Version: https://uicard.io/products/green-paper-pro
+
+==============================================
+* Coded By : UICardio https://uicard.io
+==============================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+* Copyright 2018 UICardio
+*/
+
+$(document).ready(function(){
+  
+  //Activating tooltip
+  $('[data-toggle="tooltip"]').tooltip();
+
+  //Smooth scroll
+  $('a').click(function(){
+    var hash = $(this).attr('href').split('#')[1];
+    $('html, body').animate({
+        scrollTop: $('#' + hash).offset().top
+    }, 500);
+    return false;
+  });
+
+});
